b591510a486de194a01e11f264afc2b30dfefa9f
chebetyegoh <chebetyegoh@gmail.com>
2022-09-13 01:07:15 +0300

    second commit

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,17 @@
+from re import L
 from django.urls import path
-from .views import Home
+from .views import Home, Register_student, Login_student, Register_officer,Login_officer,Login_admin,Raise_Ticket,My_Tickets,My_Account,Change_Password
 
 urlpatterns =[
     path('', Home.as_view()),
+    path('register', Register_student.as_view()),
+    path('login', Login_student.as_view()),
+    path('register-officer', Register_officer.as_view()),
+    path('login-officer', Login_officer.as_view()),
+    path('login-admin',Login_admin.as_view()),
+    path('raise-ticket', Raise_Ticket.as_view()),
+    path('my-tickets', My_Tickets.as_view()),
+    path('my-account', My_Account.as_view()),
+    path('change-password', Change_Password.as_view()),
+
 ]
--- base/views.py
+++ base/views.py
@@ -4,4 +4,33 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class Home(TemplateView):
-    template_name = 'base.html'
+    template_name = 'home.html'
+
+class Register_student(TemplateView):
+    template_name = 'student/register.html'
+
+class Login_student(TemplateView):
+    template_name = 'student/login.html'
+
+class Register_officer(TemplateView):
+    template_name = 'officer/register.html'
+
+class Login_officer(TemplateView):
+    template_name = 'officer/login.html'
+
+class Login_admin(TemplateView):
+    template_name = 'admin/login.html'
+
+class Raise_Ticket(TemplateView):
+    template_name = 'student/dashboard.html'
+
+class My_Tickets(TemplateView):
+    template_name = 'student/my_tickets.html'
+
+class My_Account(TemplateView):
+    template_name = 'student/my_account.html'
+
+class Change_Password(TemplateView):
+    template_name = 'student/change_password.html'
+
+
